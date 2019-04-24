9a115acf0bdec89f3709d7d78b1dcde6b888099e
qamar <qamarkhoza@gmail.com>
2019-04-24 19:20:49 +0200

    updated forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,50 @@
 class ArticlesController < ApplicationController
-def new
-    @article = Article.new
+    def new
+      @article = Article.new
+     end
+     before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-end
-def create
-  @article = Article.new(article_params)
-  if @article.save
-   flash[:notice] = "Article was successfully created"
-   redirect_to article_path(@article)
-  else
-   render 'new'
+     def create
+         @article = Article.new(article_params)
+         if @article.save
+         flash[:notice] = "Article was successfully created"
+         redirect_to article_path(@article)
+        else
+         render 'new'
+        end
+     end
+ private def article_params
+    params.require(:article).permit(:title, :description)
+   end
+   def show
+   @article = Article.find(params[:id])
   end
-private def article_params
-   params.require(:article).permit(:title, :description)
+
+  def edit
+        @article = Article.find(params[:id])
   end
+  def update
+      @article = Article.find(params[:id])
+   if @article.update(article_params)
+    flash[:notice] = "Article was updated"
+    redirect_to article_path(@article)
+   else
+    flash[:notice] = "Article was not updated"
+    render 'edit'
+   end
 
-end
+  end
+  def index
+      @articles = Article.all
 
-
-end
+  end
+  def destroy
+   @article = Article.find(params[:id])
+   @article.destroy
+   flash[:notice] = "Article was deleted"
+   redirect_to articles_path
+  end
+  def set_article
+    @article = Article.find(params[:id])
+   end
+ end
