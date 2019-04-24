53d8db3e519eadb936f48baa6b2c27a17bf72621
qamar <qamarkhoza@gmail.com>
2019-04-24 22:55:15 +0200

    added new controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_183530) do
+ActiveRecord::Schema.define(version: 2019_04_24_204823) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_04_24_183530) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
